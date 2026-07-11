596c2dccf99690340fecc8dac62bd101d2c7345d
KA11ZER <52335591+ka11zer@users.noreply.github.com>
2026-07-11 14:52:20 +0530

    Refactor HardcoverPlugin constructor and properties

--- HardcoverPlugin.cs
+++ HardcoverPlugin.cs
@@ -1,38 +1,27 @@
+namespace Jellyfin.Plugin.Hardcover;
+
 using System;
 using System.Collections.Generic;
+using Jellyfin.Plugin.Hardcover.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using Jellyfin.Plugin.Hardcover.Configuration;
-using MediaBrowser.Controller.Providers;
-using Jellyfin.Plugin.Hardcover.Providers;
-using Microsoft.Extensions.DependencyInjection;
-using MediaBrowser.Controller.Entities;
-using Jellyfin.Plugin.Hardcover.Api;
-using Microsoft.Extensions.Logging;
-
-namespace Jellyfin.Plugin.Hardcover;
 
 public class HardcoverPlugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
-    public HardcoverPlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, IServiceCollection services)
+    public HardcoverPlugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
         : base(applicationPaths, xmlSerializer)
     {
         Instance = this;
-        
-        // Register providers directly
-        services.AddSingleton<ILoggerFactory, LoggerFactory>();
-        services.AddSingleton<IMetadataProvider<Book>, HardcoverBookProvider>();
-        services.AddSingleton<IMetadataProvider<Person>, HardcoverPersonProvider>();
-        services.AddSingleton<IRemoteImageProvider, HardcoverBookImageProvider>();
     }
 
-    public static HardcoverPlugin? Instance { get; private set; }
+    public override string Name => "Hardcover Book Metadata";
 
-    public override string Name => "Hardcover";
-    public override string Description => "Fetches metadata for books and authors from Hardcover.";
-    public override Guid Id => Guid.Parse("a1b2c3d4-e5f6-7890-1234-567890abcdef"); // ← Use your actual plugin GUID!
+    // Static GUID generated specifically for your Hardcover implementation
+    public override Guid Id => Guid.Parse("d6b8f36c-94df-4fa3-94df-58076612df21");
+
+    public static HardcoverPlugin? Instance { get; private set; }
 
     public IEnumerable<PluginPageInfo> GetPages()
     {
@@ -41,7 +30,7 @@ public class HardcoverPlugin : BasePlugin<PluginConfiguration>, IHasWebPages
             new PluginPageInfo
             {
                 Name = "Hardcover",
-                EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                EmbeddedResourcePath = string.Format("{0}.Configuration.configPage.html", GetType().Namespace)
             }
         };
     }
